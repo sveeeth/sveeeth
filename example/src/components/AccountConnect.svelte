a94f8e0b479577a92b899e6ad1a781d91e9110fb
0xbarbs <0xbarbs@proton.me>
2023-12-14 18:23:25 +0000

    feat: add account connection status to example

--- example/src/components/AccountConnect.svelte
+++ example/src/components/AccountConnect.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
   import { account, connect, disconnect } from "sveeeth";
   import { InjectedConnector } from "sveeeth/connectors";
+
+  $: ({ address, isConnected, status } = $account);
 </script>
 
-<h2>Account</h2>
-{#if !$account.isConnected}
+<h2>Account ({status})</h2>
+
+{#if !isConnected}
   <button on:click={() => connect({ connector: new InjectedConnector() })}>Connect</button>
 {:else}
   <p>
-    address: {$account.address}
+    address: {address}
     <button on:click={disconnect}>Disconnect</button>
   </p>
 {/if}
