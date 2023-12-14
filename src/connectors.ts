d1950c26e7d7adfcd3bb84a21801ab402d13b4d5
0xbarbs <0xbarbs@proton.me>
2023-12-14 17:48:02 +0000

    fix: export all connectors from @wagmi/core

--- src/connectors.ts
+++ src/connectors.ts
@@ -1,1 +1,8 @@
 export * from "@wagmi/core/connectors";
+export * from "@wagmi/core/connectors/coinbaseWallet";
+export * from "@wagmi/core/connectors/metaMask";
+export * from "@wagmi/core/connectors/mock";
+export * from "@wagmi/core/connectors/safe";
+export * from "@wagmi/core/connectors/walletConnect";
+export * from "@wagmi/core/connectors/walletConnectLegacy";
+
